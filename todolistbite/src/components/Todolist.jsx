d0ef290f657ff3a6f8f5b601a05ca3d6c0bfa580
Liisa Davydov <liisamuttonen@gmail.com>
2024-10-07 15:40:58 +0300

    Add Datepicker from MUI

--- todolistbite/src/components/Todolist.jsx
+++ todolistbite/src/components/Todolist.jsx
@@ -1,118 +1,147 @@
 import { useState, useRef } from "react";
-import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
+import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the Data Grid
-
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
 function Todolist() {
-    const [todo, setTodo] = useState({ description: "", duedate: "", priority: ""});
-    const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState({
+    description: "",
+    duedate: "",
+    priority: "",
+  });
+  const [todos, setTodos] = useState([]);
 
-    const gridRef = useRef();
+  const gridRef = useRef();
 
-    const [colDefs, setColDefs] = useState([
-        { 
-            field: "description", 
-            filter: true, 
-            editable: true, 
-            floatingFilter: true
-        },
-        { 
-            field: "priority", 
-            sortable: true, 
-            filter: true,
-            floatingFilter: true,
-            cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'} 
-        },
-        { 
-            field: "duedate", 
-            sortable: true, 
-            filter: true, 
-            floatingFilter: true
-        }
-        //cellRender delete
-       // {
-       //     headerName: 'Actions', 
-       //     cellRenderer: (params) => (
-       //         <button onClick={() => handleDelete(params.node.rowIndex)}>
-       //             Delete
-       //         </button>
-       //     )
-       // }
-    ])
+  const columns = [
+    {
+      field: "description",
+      filter: true,
+      editable: true,
+      floatingFilter: true,
+    },
+    {
+      field: "priority",
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+      cellStyle: (params) =>
+        params.value === "High" ? { color: "red" } : { color: "black" },
+    },
+    {
+      field: "duedate",
+      sortable: true,
+      filter: true,
+      floatingFilter: true,
+    },
+  ];
 
-    const handleAdd = () => {
-        console.log(todo)
-        if (!todo.description || !todo.priority || !todo.duedate)  {
-            alert("Type something first")
-        } else {
-            //adding todo to the beginning and not replacing state
-            setTodos(todos => [todo, ...todos]);
-            setTodo({ description: "", duedate: "", priority: "" });
-        }
+  const handleAdd = () => {
+    console.log(todo);
+    if (!todo.description || !todo.priority || !todo.duedate) {
+      alert("Type something first");
+    } else {
+      //adding todo to the beginning and not replacing state
+      setTodos((todos) => [todo, ...todos]);
+      setTodo({ description: "", duedate: "", priority: "" });
     }
+  };
 
-    const handleDelete = () => {
-        if (gridRef.current.getSelectedNodes().length > 0) {
-        setTodos(todos.filter((_, index) =>
-        gridRef.current.getSelectedNodes()[0].id != index));
+  const handleDelete = () => {
+    if (gridRef.current.getSelectedNodes().length > 0) {
+      setTodos(
+        todos.filter(
+          (_, index) => gridRef.current.getSelectedNodes()[0].id != index
+        )
+      );
+    } else {
+      alert("Select a row first!");
     }
-        else {
-            alert('Select a row first!');
-        }
-    }
+  };
+  //ask about color
+  const handleDateChange = (date) => {
+    setTodo({ ...todo, duedate: dayjs(date).toISOString().substring(0, 10) });
+  };
 
-        //if (gridRef.current.getSelectedNodes().length > 0) {
-        //setTodos(todos.filter((todo, index) => 
-        //   index != gridRef.current.getSelectedNodes()[0].id))
-        //}
-        //else {
-        //    alert('Select a row first!');
-        //}
-        //  };
-
-        //console.log("Delete" + rowIndex);
-        //boolean check, if index is equal: delete
-        //setTodos(todos => todos.filter((todo, i) => i !== rowIndex));
-        //setTodos(array);
-        //slice or filter usage to delete index value fom an array
-    //}
-
-    return (
-        <>
-            <h3>My Todos</h3>
-            <input
-                placeholder="Description"
-                value={todo.description}
-                onChange={event => setTodo({ ...todo, description: event.target.value })}
-            />
-            <input
-                placeholder="Priority"
-                value={todo.priority}
-                onChange={event => setTodo({ ...todo, priority: event.target.value })}
-            />
-            <input
-                type="date"
-                value={todo.duedate}
-                onChange={event => setTodo({ ...todo, duedate: event.target.value })}
-            />
-            <button onClick={handleAdd}>Add Todo</button>
-            <button onClick={handleDelete}>Delete</button>
-            <div
-                className="ag-theme-material" // applying the Data Grid theme
-                style={{ height: 500 }} // the Data Grid will fill the size of the parent container
-            >
-                <AgGridReact
-                    ref={gridRef}
-                    onGridReady={ params => gridRef.current = params.api }
-                    rowData={todos}
-                    columnDefs={colDefs}
-                    rowSelection='single'
-                    animateRows={true}
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <h3></h3>
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+        mt={2}
+      >
+        <TextField
+          variant="standard"
+          label="Description"
+          value={todo.description}
+          onChange={(event) =>
+            setTodo({ ...todo, description: event.target.value })
+          }
+        />
+        <TextField
+          variant="standard"
+          label="Priority"
+          value={todo.priority}
+          onChange={(event) =>
+            setTodo({ ...todo, priority: event.target.value })
+          }
+        />
+        <TextField
+          variant="standard"
+          label="date"
+          value={todo.duedate}
+          onChange={(event) =>
+            setTodo({ ...todo, duedate: event.target.value })
+          }
+        />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Due Date"
+            value={todo.duedate ? dayjs(todo.duedate) : null}
+            onChange={handleDateChange}
+            renderInput={(params) => (
+              <TextField variant="standard" {...params} />
+            )}
+          />
+        </LocalizationProvider>
+        <Button variant="contained" onClick={handleAdd}>
+          Add Todo
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          endIcon={<DeleteIcon />}
+          onClick={handleDelete}
+        >
+          Delete
+        </Button>
+      </Stack>
+      <div
+        className="ag-theme-material" // applying the Data Grid theme
+        style={{ height: 500 }} // the Data Grid will fill the size of the parent container
+      >
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={(params) => (gridRef.current = params.api)}
+          rowData={todos}
+          columnDefs={columns}
+          rowSelection="single"
+          animateRows={true}
+        />
+      </div>
+    </>
+  );
 }
 
 export default Todolist;
